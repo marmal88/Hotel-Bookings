9d28a191c2432ea1c8966379fa60d001558d638b
marmal88 <dlow017@e.ntu.edu.sg>
2022-06-17 11:24:52 +0800

    added unit test for extract

---
+++ tests/test_extract.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import pytest
+
+from src.config.config_load import read_yaml_file
+from src.extract import ImportData
+
+@pytest.fixture
+def fixture_extract():
+    """Pytest fixture for ImportData Object
+    Returns:
+        fixture_extract (object): returns ImportData object
+    """
+    config = read_yaml_file()
+    data_location = config["data"]["data_location"]
+    main = ImportData(data_location)
+    return main
+
+def test_dataframe(fixture_extract):
+    """Testing to see if return is indeed dataframe object type
+    Args:
+        fixture_extract (object): ImportData object
+    """
+    config = read_yaml_file()
+    data_table = config["data"]["data_table"]
+    assert isinstance(fixture_extract.return_table(data_table), pd.DataFrame)
