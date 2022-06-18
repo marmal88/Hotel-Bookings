b160ec7e10de4ca1c7e45d56540b7ae1dfcba314
marmal88 <dlow017@e.ntu.edu.sg>
2022-06-18 18:03:39 +0800

    finalized running format for .sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python src/app.py
+python -m src.app
--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
-from preprocess import Preprocess
-from mlpipe import MLpipeline
+from src.preprocess import Preprocess
+from src.mlpipe import MLpipeline
+from src.config.config_load import read_yaml_file
 
-df = Preprocess().preprocess_df()
-MLpipeline().ml_workflow(df, "lr")
-MLpipeline().ml_workflow(df, "dt")
-MLpipeline().ml_workflow(df, "rf")
+if __name__=="__main__":
+    df = Preprocess().preprocess_df()
+    MLpipeline().ml_workflow(df, "lr")
+    MLpipeline().ml_workflow(df, "dt")
+    MLpipeline().ml_workflow(df, "rf")
+    print("Run completed please see results in output folder")
